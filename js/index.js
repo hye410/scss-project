2f33861d4ee53c56c58c4849369a6c29db3b3854
hye410 <dhye410@daum.net>
2023-03-06 22:20:13 +0900

    bootStrap

---
+++ js/index.js
@@ -0,0 +1,79 @@
+import $ from "jquery";
+// import tip from './index2';
+// tip()
+$(function(){
+  // $('[data-toggle="tooltip"]').tooltip();
+
+  $('ul.all>li').on('mouseenter',function(e){    
+    $('.prev').css('cursor','pointer');
+    $('.next').css('cursor','pointer');
+  })
+
+  // gallery 준비하기
+  const wli = $('ul.all>li').width();
+  // console.log(wli)
+  $('ul.all>li:last').prependTo('ul.all');
+  $('ul.all').css('margin-left','-'+wli+'px');
+
+  // $('ul.all>li').on('mouseleave',function(e){
+  //   $('.prev').css('display','none');
+  //   $('.next').css('display','none');
+  // })
+  
+  timer();
+  setInterval(timer,3000);
+  ani();
+  btn();
+})
+
+
+
+//  aside event
+
+
+
+function ani(){
+  const wli = $('ul.all>li').width();
+  $('.slide>.prev').on('click',function(e){
+    $('ul.all').animate({marginLeft:'+='+wli+'px'},
+    function(){
+    $('ul.all>li:last').prependTo('ul.all');
+    $('ul.all').css('margin-left','-'+wli+'px');
+    })
+  })
+ 
+  $('.slide>.next').on('click',function(e){
+    $('ul.all').animate({marginLeft:'-='+wli+'px'},
+    function(){
+      $('ul.all').css('margin-left','-'+wli+'px');
+      $('ul.all>li:first').appendTo('ul.all');
+    })    
+  })
+}
+  
+function timer(){
+  const wli = $('ul.all>li').width();
+    $('ul.all').animate({marginLeft:'-='+wli+'px'},
+    function(){
+      $('ul.all').css('margin-left','-'+wli+'px');
+      $('ul.all>li:first').appendTo('ul.all');
+    })
+}
+
+function btn(){
+  $('.slide').on('mouseenter',function(e){
+    $('.prev').css('display','block')
+    $('.next').css('display','block')
+  })
+  $('.slide').on('mouseleave',function(e){
+    $('.prev').css('display','none')
+    $('.next').css('display','none')
+  })
+}
+
+// bootstrap
+
+
+
+
+  
---
+++ js/index2.js
@@ -0,0 +1,6 @@
+function tip(){
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+}
+
+export default tip;
